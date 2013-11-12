6075f8e5c29cf8e613e39542fe67c13304b87444
Rene <minet.ws@gmail.com>
2013-11-12 14:16:27 -0400

    agregar view + css

--- public/js/clases/Pokemon.js
+++ public/js/clases/Pokemon.js
@@ -4,8 +4,12 @@ angular
   .factory('Pokemon', function() {
     function Pokemon(data) {
       var predeterminados = {
-        imagen  : 'http://imageshack.com/a/img440/3812/kzu.png',
-        tipos   : []
+        imagen    : 'http://imageshack.com/a/img440/3812/kzu.png',
+        tipos     : [],
+        ataque    : 0,
+        defensa   : 0,
+        ataqueSP  : 0,
+        defensaSP : 0
       };
       
       angular.extend(this, predeterminados, data);
